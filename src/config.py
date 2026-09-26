780f22da8e429e4b31efe8ec79a67229ac537ffc
alchemistC137 <marcomemba@outlook.es>
2026-09-26 19:56:34 +0200

    Ajuste variables irrelevantes.

--- src/config.py
+++ src/config.py
@@ -11,21 +11,10 @@ PROCESSED_DATA_PATH = ROOT_PATH / "data" / "processed" / "dataset_practica_final
 OUTPUTS_DIR = ROOT_PATH / "outputs"
 TARGET_COLUMN = 'is_canceled'
 IRRELEVANT_COLUMNS = [
-    'meal',
-    'total_of_special_requests',
-    'required_car_parking_spaces',
-    'days_in_waiting_list',
-    'arrival_date_year',
-    'arrival_date_month',
-    'arrival_date_day_of_month',
-    'arrival_date_week_number',
-    'reservation_status', 
-    'reservation_status_date',
     'assigned_room_type',
+    'booking_changes',
     'reservation_status',
-    'reservation_status_date', 
-    'assigned_room_type', 
-    'country' 
+    'reservation_status_date'
 ]
 
 DICT_CAST_CATEGORY_COLS = {
