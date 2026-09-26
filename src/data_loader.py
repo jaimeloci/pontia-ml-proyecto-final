e836b9f89ba5e17c50b86045196ae4e3ce043f84
alchemistC137 <marcomemba@outlook.es>
2026-09-26 19:51:49 +0200

    Ajuste exportacion fichero

--- src/data_loader.py
+++ src/data_loader.py
@@ -24,11 +24,7 @@ def preparar_datos(df: pd.DataFrame) -> pd.DataFrame:
     return df_clean
 
 def generar_csv_datos_preprocesados(df: pd.DataFrame):
-<<<<<<< HEAD
-    df.to_csv(PROCESSED_DATA_PATH, index=False)
-=======
     df_preprocessed = df
     os.makedirs(os.path.dirname(PROCESSED_DATA_PATH), exist_ok=True)
     df_preprocessed.to_csv(PROCESSED_DATA_PATH, index=False)
 
->>>>>>> 2960fd1c68fdacd6f929fb6f566d35f1c6c1d79f
